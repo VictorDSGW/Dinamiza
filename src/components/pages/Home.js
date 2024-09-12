9dbdc9b0662e598f61057bbb2a501461ff8a2f7b
Victor DSG <victor.dsgw@gmail.com>
2024-09-12 18:28:39 -0300

    Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,15 @@
+import styles from './Home.module.css'
+import timeManagement from '../../img/time-schedule-management-business-svgrepo-com.png'
+import LinkButton from '../layout/LinkButton'
+
 function Home() {
   return (
-    <h1>Home</h1>
+    <section className={styles.home_container}>
+      <h1>Bem-vindo ao <span>Dinamiza</span></h1>
+      <p>Gerencie seus projetos de forma eficiente!</p>
+      <LinkButton to='newproject' text='Criar Projeto' />
+      <img src={timeManagement} alt='Dinamiza' />
+    </section>
   )
 }
 
